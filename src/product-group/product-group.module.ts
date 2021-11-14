28c5ccbe2126427af7f4fe5446bc56a9cf726c4c
DzmitrySakalouski <dmitry89sokol@gmail.com>
2021-11-14 13:40:27 +0300

    fix" deps

--- src/product-group/product-group.module.ts
+++ src/product-group/product-group.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductGroupService } from './product-group.service';
 import { ProductGroupController } from './product-group.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductGroup } from './model/product-group.model';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductGroup])],
   providers: [ProductGroupService],
   controllers: [ProductGroupController]
 })
